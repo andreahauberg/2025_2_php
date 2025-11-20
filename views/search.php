6a81aacad486ad7b85599e54db4ed026e65fd1a4
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-11-20 16:19:33 +0100

    changhed classnames

--- views/search.php
+++ views/search.php
@@ -1,24 +1,24 @@
 <div class="search-results">
 
-    <section class="search-results__section">
-        <h3 class="search-results__title">Users</h3>
+    <section class="search-results-section">
+        <h3 class="search-results-title">Users</h3>
 
         <?php if (empty($users)): ?>
             <p>No users found</p>
         <?php else: ?>
-            <ul class="search-results__list">
+            <ul class="search-results-list">
                 <?php foreach ($users as $user): ?>
-                    <li class="search-results__user">
+                    <li class="search-results-user">
 
                         <div>
                             <span
-                                class="search-results__user-name"
+                                class="search-results-user-name"
                                 data-search-text="<?php echo htmlspecialchars($user["user_full_name"]); ?>">
                                 <?php echo htmlspecialchars($user["user_full_name"]); ?>
                             </span>
 
                             <span
-                                class="search-results__user-handle"
+                                class="search-results-user-handle"
                                 data-search-text="@<?php echo htmlspecialchars($user["user_username"]); ?>">
                                 @<?php echo htmlspecialchars($user["user_username"]); ?>
                             </span>
@@ -30,27 +30,27 @@
         <?php endif; ?>
     </section>
 
-    <section class="search-results__section">
-        <h3 class="search-results__title">Posts</h3>
+    <section class="search-results-section">
+        <h3 class="search-results-title">Posts</h3>
 
         <?php if (empty($posts)): ?>
             <p>No posts found</p>
         <?php else: ?>
-            <div class="search-results__grid">
+            <div class="search-results-grid">
 
 <?php foreach ($posts as $post): ?>
-    <article class="search-results__post-card">
+    <article class="search-results-post-card">
 
         <?php if (!empty($post["post_image_path"])): ?>
-            <div class="search-results__post-image-wrapper">
+            <div class="search-results-post-image-wrapper">
                 <img src="<?php echo htmlspecialchars($post["post_image_path"]); ?>"
-                     class="search-results__post-image">
+                     class="search-results-post-image">
             </div>
         <?php endif; ?>
 
-        <div class="search-results__post-body">
+        <div class="search-results-post-body">
 
-            <div class="search-results__post-author">
+            <div class="search-results-post-auhtor">
 
 <span
     data-search-text="@<?php echo htmlspecialchars($post["user_username"]); ?>">
@@ -60,7 +60,7 @@
             </div>
 
             <p
-                class="search-results__post-text"
+                class="search-results-post-text"
                 data-search-text="<?php echo htmlspecialchars($post["post_message"]); ?>">
                 <?php echo htmlspecialchars($post["post_message"]); ?>
             </p>
