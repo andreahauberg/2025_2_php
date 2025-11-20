517f642386ac4dad5f5b3575a0e83b6daaba7ec1
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-11-20 15:43:33 +0100

    added js link to search.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,8 @@ import { burgerMenu } from "./burger-menu.js";
 
 import { flipBtn } from "./flip-btn.js";
 
+import { searchOverlay } from "./search.js";
+
 burgerMenu();
 flipBtn();
+searchOverlay();
