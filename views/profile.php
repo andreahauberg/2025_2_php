2ea305cd51ec438d920234f1aa6d1a913a0252ee
Sofia S. Rokkedal <ssimone12@gmail.com>
2025-11-26 16:25:07 +0100

    fixed  delete post on profile

--- views/profile.php
+++ views/profile.php
@@ -93,6 +93,7 @@ $usersToFollow = $stmt->fetchAll();
     <script defer src="../public/js/dialog.js"></script>
     <script defer src="../public/js/flip-btn.js"></script>
     <script defer src="../public/js/comment.js"></script>
+    <script defer src="../public/js/confirm-delete.js"></script>
     <title>Profile: <?php echo htmlspecialchars($currentUser["user_full_name"]); ?></title>
 </head>
 <body>
