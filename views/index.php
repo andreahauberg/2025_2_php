c67be1922648fadffbd00643eebfa20735c62517
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-12-04 16:01:22 +0100

    favicon change

--- views/index.php
+++ views/index.php
@@ -7,7 +7,7 @@ $title = "Welcome";
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= htmlspecialchars($title) ?></title>
-  <link rel="icon" type="image/x-icon" href="/public/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="/public/favicon/favicon.ico">
   <link rel="stylesheet" href="/public/css/app.css">
   <link rel="stylesheet" href="/public/css/search.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
