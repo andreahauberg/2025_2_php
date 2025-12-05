94a38cd388096eaae1237f3da0d69dd7676644a3
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-12-05 12:17:16 +0100

    added script

--- views/index.php
+++ views/index.php
@@ -18,6 +18,7 @@ $title = "Welcome";
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <script type="module" src="/public/js/app.js"></script>
   <script defer src="/public/js/dialog.js"></script>
+  <script src="/public/js/validation.js" mix-ignore></script>
 </head>
 <body>
 
